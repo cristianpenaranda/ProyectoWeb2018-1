36e43f464c37d0de708c4921fe7d8a846479c329
Cristian Peñaranda <37253214+cristianpenaranda@users.noreply.github.com>
2018-06-13 07:00:05 -0500

    Add files via upload

--- view/plantilla.php
+++ view/plantilla.php
@@ -7,11 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
+    <link rel="icon" type="image/png" href="https://drive.google.com/uc?export=view&id=1WjtOJT6Eq1PoZPulS_dL4EeG3Hu82W7n"/>
+    
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
@@ -19,17 +20,21 @@
 
     <script type="text/javascript" src="view/presentacion/js/jquery-3.3.1.min.js"></script>
 	
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">    
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> 
+    <script src="https://unpkg.com/ionicons@4.1.2/dist/ionicons.js"></script>
     <script src ="https://unpkg.com/sweetalert/dist/sweetalert.min.js "></script>
     
     <script type="text/javascript" src="view/presentacion/js/jquery.validate.js"></script>
+    
 
     <!--ARCHIVOS PERSONALES-->
     <link rel="stylesheet" href="view/presentacion/css/estilos.css">
     <script type="text/javascript" src="view/presentacion/js/flechaSubir.js"></script>
     <script type="text/javascript" src="view/presentacion/js/alertas.js"></script>
     <script type="text/javascript" src="view/presentacion/js/inicio.js"></script>
-    <script type="text/javascript" src="view/presentacion/js/registroFuncionario.js"></script>
+    <script type="text/javascript" src="view/presentacion/js/perfil.js"></script>
+    <script type="text/javascript" src="view/presentacion/js/registros.js"></script>
+    <script type="text/javascript" src="view/presentacion/js/registros_formato.js"></script>
     <script type="text/javascript" src="view/presentacion/js/salir.js"></script>
     <script type="text/javascript" src="view/presentacion/js/recuperarContraseña.js">
     </script>
