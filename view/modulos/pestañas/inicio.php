2725f878b87b499ea296ea3fde4f33933b004f9a
Cristian Peñaranda <37253214+cristianpenaranda@users.noreply.github.com>
2018-06-13 07:00:51 -0500

    Add files via upload

--- view/modulos/pestañas/inicio.php
+++ view/modulos/pestañas/inicio.php
@@ -1,84 +1,84 @@
 <?php
-    if(isset($_SESSION["Usuario"])){
-        header("Location:Perfil");
-    }
+if (isset($_SESSION["Usuario"])) {
+    header("Location:Perfil");
+}
 ?>
 
 <div class="estiloInicio">
-	<br>
-	<div class="col-md-5" style="display: block;margin: auto;">
-	 	<form class="login" id="FormLogin" method="POST">
-	 		<img src="https://upload.wikimedia.org/wikipedia/commons/e/ef/Imagen-logo-white-on-orange2.png">
-	 		<hr>
-	 		<p>Ingresa tus datos para iniciar sesión</p>
-	 		<div class="col-auto">
-                        
-                            <div class="form-group input-group">
+    <br>
+    <div class="col-md-5" style="display: block;margin: auto;">
+        <form class="login" id="FormLogin" method="POST">
+            <img src="https://drive.google.com/uc?export=view&id=1VqeLGaJjen87r1GnOV_VTt9Ds4jHSyf3" style="width: 95%;border: 2px double #007bff;border-radius: 15px 15px 15px 15px;">
+            <hr>
+            <p>Ingresa tus datos para iniciar sesión</p>
+            <div class="col-auto">
+
+                <div class="form-group input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text ion-person" for="ingresarDocumento" id="basic-addon1"></span>
+                    </div>
+                    <input type="number" name="ingresarUsuario" class="form-control" placeholder="Usuario" id="ingresarUsuario" required>
+                </div>
+
+                <div class="form-group input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text ion-locked" for="ingresarContraseña" id="basic-addon1"></span>
+                    </div>
+                    <input type="password" name="ingresarContraseña" maxlength="10" class="form-control" placeholder="Contraseña" id="ingresarContraseña" required>
+                </div>
+
+                <div class="form-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text ion-person-stalker" id="basic-addon1" for="ingresarTipo"></span>       
+                        <select id="ingresarTipo" name="ingresarTipo" class="form-control" required> 
+                            <option value="">Seleccione tipo de usuario</option>
+                            <option value="Administrador">Administrador</option>
+                            <option value="Funcionario">Funcionario</option>
+                        </select>
+                        <label class="bg-danger" id="error-tipoUsuarioI"></label>     
+                    </div>
+                </div>
+
+                <div class="input-group mb-3">
+                    <button type="submit" class="btn btn-primary"><span class="ion-log-in"></span> Ingresar</button>
+                </div>
+
+                <div class="form-group">
+                    <a  href="" data-toggle="modal" data-target=".bd-example-modal-sm" id="modal"><span class="ion-help-circled"></span> Has olvidado tu contraseña?</a>
+                </div>
+
+            </div>
+            <br>
+        </form>
+
+
+
+        <!--MODAL RECUPERAR CONTRAASEÑA-->
+        <form class="modal fade bd-example-modal-sm modalContraseña" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" id="formRecordar" method="POST">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalCenterTitle">Recordar Contraseña <span class="ion-loop"></span></h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group input-group" style="color:black">
                             <div class="input-group-prepend">
-                                <span class="input-group-text ion-person" for="ingresarDocumento" id="basic-addon1"></span>
+                                <span class="input-group-text ion-person" for="recordarUsuario" id="basic-addon1"></span>
                             </div>
-                            <input type="number" name="ingresarUsuario" class="form-control" placeholder="Usuario" id="ingresarUsuario" required>
+                            <input type="number" name="recordarUsuario" class="form-control" placeholder="Usuario" id="recordarUsuario" required>
                         </div>
+                    </div>
 
-                        <div class="form-group input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text ion-locked" for="ingresarContraseña" id="basic-addon1"></span>
-                            </div>
-                            <input type="password" name="ingresarContraseña" maxlength="10" class="form-control" placeholder="Contraseña" id="ingresarContraseña" required>
-                        </div>
-                          
-			<div class="form-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text ion-person-stalker" id="basic-addon1" for="ingresarTipo"></span>       
-                                <select id="ingresarTipo" name="ingresarTipo" class="form-control" required> 
-                                    <option value="">Seleccione tipo de usuario</option>
-                                    <option value="Administrador">Administrador</option>
-                                    <option value="Funcionario">Funcionario</option>
-                                </select>
-                                <label class="bg-danger" id="error-tipoUsuarioI"></label>     
-                            </div>
-                        </div>
+                    <div class="modal-footer" style="display: block;margin: auto;">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar <span class="ion-close-round"></span></button>
+                        <button type="submit" class="btn btn-primary">Recordar <span class="ion-loop"></span></button>
+                    </div>
+                </div>
+            </div>
+        </form>
 
- 				<div class="input-group mb-3">
-                                    <button type="submit" class="btn btn-primary"><span class="ion-log-in"></span> Ingresar</button>
-		     	</div>
-
-		     	<div class="form-group">
-                            <a  href="" data-toggle="modal" data-target=".bd-example-modal-sm" id="modal"><span class="ion-help-circled"></span> Has olvidado tu contraseña?</a>
-                        </div>
-
-		    </div>
-		    <br>
-	 	</form>
-	 	
-
-
-                    <!--MODAL RECUPERAR CONTRAASEÑA-->
-                    <form class="modal fade bd-example-modal-sm modalContraseña" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" id="formRecordar" method="POST">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalCenterTitle">Recordar Contraseña <span class="ion-loop"></span></h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <div class="form-group input-group" style="color:black">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text ion-person" for="recordarUsuario" id="basic-addon1"></span>
-                                            </div>
-                                            <input type="number" name="recordarUsuario" class="form-control" placeholder="Usuario" id="recordarUsuario" required>
-                                        </div>
-                                    </div>
-
-                                    <div class="modal-footer" style="display: block;margin: auto;">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar <span class="ion-close-round"></span></button>
-                                        <button type="submit" class="btn btn-primary">Recordar <span class="ion-loop"></span></button>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-
-	</div>
-	<br>
+    </div>
+    <br>
 </div>
 
--- view/modulos/pestañas/perfil.php
+++ view/modulos/pestañas/perfil.php
@@ -12,7 +12,7 @@ echo '<div class="container fondoApp">
                 <img src="https://drive.google.com/uc?export=view&id=1VAVdqLOT3aPd8XqyhcBVkV9TVa3n3A-z" alt="" class="img-rounded img-responsive" />
                 <div class="info">
                     <h3><span class="ion-person"></span> ' .$user->getNombre().' '.$user->getApellidos().'</h3>
-                    <h5><span class="ion-card"></span> '.$user->getCedula().'</h5>
+                    <h5 id="UsuarioCambiarContraseña"><span class="ion-card"></span> '.$user->getCedula().'</h5>
                     <span class="ion-location"></span> '.$user->getDireccion().'
                     <p>
                         <span class="ion-email"></span> '.$user->getCorreo().'
@@ -29,34 +29,29 @@ echo '<div class="container fondoApp">
 </div>
 
 <!--MODAL CAMBIAR CONTRAASEÑA-->
-<form class="modal fade bd-example-modal-sm modalContraseña" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" id="formCambiar" method="POST">
+<form class=" modal fade bd-example-modal-sm modalContraseña" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" id="FormCambiarContraseña" method="POST">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalCenterTitle">Cambiar Contraseña </h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
-            </div>
+            <h5 class="modal-title mt-3 mb-3" id="exampleModalCenterTitle" style="text-align: center;">Cambiar Contraseña </h5>
+            <hr style="width:90%;">
             <div class="modal-body">
                 <div class="form-group input-group" style="color:black">
                     <div class="input-group-prepend">
-                        <span class="input-group-text ion-unlocked" for="cambiarClave1" id="basic-addon1"></span>
+                        <span class="input-group-text ion-unlocked" for="AnteriorCambiarContraseña" id="basic-addon1"></span>
                     </div>
-                    <input type="text" name="cambiarClave1" class="form-control" placeholder="Contraseña Anterior" id="cambiarClave1" required>
+                    <input type="password" maxlength="50"  class="form-control" placeholder="Contraseña Anterior" id="AnteriorCambiarContraseña" name="AnteriorCambiarContraseña" required>
                 </div>
 
                 <div class="form-group input-group"  style="color:black">
                     <div class="input-group-prepend">
-                        <span class="input-group-text ion-locked" for="cambiarClave2" id="basic-addon1"></span>
+                        <span class="input-group-text ion-locked" for="NuevaCambiarContraseña" id="basic-addon1"></span>
                     </div>
-                    <input type="text" name="cambiarClave2" class="form-control" placeholder="Contraseña Nueva" id="cambiarClave2" required>
+                    <input type="password" maxlength="50"  class="form-control" placeholder="Contraseña Nueva" id="NuevaCambiarContraseña" name="NuevaCambiarContraseña" required>
                 </div>
-
-            </div>
-
-            <div class="modal-footer" style="display: block;margin: auto;">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar <span class="ion-close-round"></span></button>
-                <button type="submit" class="btn btn-primary">Cambiar <span class="ion-loop"></span></button>
-            </div>
+                <div class="row">
+                    <button type="submit" class="btn btn-primary ml-5 mr-3" id="botonCambiarContraseña" style="width:50%;">Cambiar <span class="ion-loop"></span></button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><span class="ion-close-round"></span> Cerrar</button></div>
+                </div>
         </div>
     </div>
 </form>'
